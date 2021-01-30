e6273c532ea43493dc2c740891c98e30dd3efc54
Mikhail <MIKHAILVALKOPYLOV@YANDEX.Ru>
2021-01-31 00:59:31 +0300

    home work 6 task 1

--- app/src/main/java/com/amk/mygithubclient/mvp/model/cash/room/RoomGithubRepositoriesCache.kt
+++ app/src/main/java/com/amk/mygithubclient/mvp/model/cash/room/RoomGithubRepositoriesCache.kt
@@ -1,6 +1,46 @@
 package com.amk.mygithubclient.mvp.model.cash.room
 
 import com.amk.mygithubclient.mvp.model.cash.IGithubRepositoriesCache
+import com.amk.mygithubclient.mvp.model.entity.GithubRepository
+import com.amk.mygithubclient.mvp.model.entity.GithubUser
+import com.amk.mygithubclient.mvp.model.entity.room.Database
+import com.amk.mygithubclient.mvp.model.entity.room.RoomGithubRepository
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.internal.operators.completable.CompletableError
 
-class RoomGithubRepositoriesCache : IGithubRepositoriesCache {
+class RoomGithubRepositoriesCache(private val db: Database) : IGithubRepositoriesCache {
+    override fun addRepository(
+        user: GithubUser,
+        repositories: List<GithubRepository>
+    ): Completable =
+        try {
+            repositories.map {
+                db.repositoryDao.insert(
+                    RoomGithubRepository(
+                        it.id,
+                        it.name ?: "",
+                        it.forksCount ?: 0,
+                        user.id,
+                        it.watchersCount ?: 0,
+                        it.language ?: ""
+                    )
+                )
+            }
+            Completable.complete()
+        } catch (e: Exception) {
+            CompletableError(e)
+        }
+
+    override fun getRepositories(user: GithubUser):Single<List<GithubRepository>> = Single.fromCallable {
+        db.repositoryDao.findForUser(user.id).map {
+            GithubRepository(
+                it.id,
+                it.name,
+                it.forksCount,
+                it.watchersCount,
+                it.language
+            )
+        }
+    }
 }
--- app/src/main/java/com/amk/mygithubclient/mvp/model/cash/room/RoomGithubUsersCache.kt
+++ app/src/main/java/com/amk/mygithubclient/mvp/model/cash/room/RoomGithubUsersCache.kt
@@ -1,6 +1,36 @@
 package com.amk.mygithubclient.mvp.model.cash.room
 
 import com.amk.mygithubclient.mvp.model.cash.IGithubUsersCache
+import com.amk.mygithubclient.mvp.model.entity.GithubUser
+import com.amk.mygithubclient.mvp.model.entity.room.Database
+import com.amk.mygithubclient.mvp.model.entity.room.RoomGithubUser
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.internal.operators.completable.CompletableError
 
-class RoomGithubUsersCache : IGithubUsersCache {
+class RoomGithubUsersCache(
+    private val db: Database,
+) : IGithubUsersCache {
+
+    override fun getUsers(): Single<List<GithubUser>> = Single.fromCallable {
+        db.userDao.getAll().map {
+            GithubUser(it.id, it.login, it.avatarUrl, it.reposUrl)
+        }
+    }
+
+    override fun addUsers(users: List<GithubUser>): Completable =
+        try {
+            val listRoomGithubUser = users.map {
+                RoomGithubUser(
+                    it.id,
+                    it.login,
+                    it.avatarUrl ?: "",
+                    it.reposUrl ?: ""
+                )
+            }
+            db.userDao.insert(listRoomGithubUser)
+            Completable.complete()
+        } catch (e: Exception) {
+            CompletableError(e)
+        }
 }
